9758e85ed1f1fee3d7b424104f695f476fc7f11b
jangdm37 <jangdm37@gmail.com>
2024-05-25 20:40:40 +0900

    ✨ feat(setup): 프로젝트 초기 설정 및 .gitignore 업데이트(#3)
    Spring Boot 프로젝트 초기 설정을 완료하고, 불필요한 파일들을 제외하기 위해 .gitignore 파일을 업데이트했습니다.
    - build.gradle 설정
    - Lombok, Devtools 및 데이터베이스 관련 의존성 추가
    - application.properties, .DS_Store 파일 무시하도록 .gitignore 파일 수정
    
    Resolves: #3

---
+++ nailmanager/src/test/java/com/nailcase/nailmanager/NailmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailcase.nailmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
