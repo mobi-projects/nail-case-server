5211c8862d116eb6b06592d9a55a8b0a4f35f010
YoonSung Kim <opqudk@gmail.com>
2024-06-04 14:42:58 +0900

    :art: style: Remove line breaks (NAILCASE-50)
    resolve: [NAILCASE-50](https://nailcase.atlassian.net/browse/NAILCASE-50)

--- src/main/java/com/nailcase/customer/repository/CustomerRepository.java
+++ src/main/java/com/nailcase/customer/repository/CustomerRepository.java
@@ -1,11 +1,10 @@
 package com.nailcase.customer.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nailcase.customer.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
 }
