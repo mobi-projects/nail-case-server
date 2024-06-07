a43dafb3dd70c7a238280f1a80f64f97e23162c8
jangdm37 <jangdm37@gmail.com>
2024-06-07 14:33:57 +0900

    ✨ feat(set-security-jwt): set-redis-security-jwt (NAILCASE-49)
    - 수정 사항 반영
    resolves: [NAILCASE-49](https://nailcase.atlassian.net/browse/NAILCASE-49)

--- src/main/java/com/nailcase/customer/domain/Role.java
+++ src/main/java/com/nailcase/customer/domain/Role.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-	OWNER("ROLE_SHOP_OWNER"), MANAGER("ROLE_SHOP_MANAGER"), GUEST("ROLE_GUEST");
+	OWNER("ROLE_SHOP_OWNER"), MANAGER("ROLE_SHOP_MANAGER"), USER("ROLE_SHOP_USER"), GUEST("ROLE_GUEST");
 	private final String key;
 
 }
--- src/main/java/com/nailcase/oauth2/CustomOAuth2User.java
+++ src/main/java/com/nailcase/oauth2/CustomOAuth2User.java
@@ -13,20 +13,13 @@ import lombok.Getter;
 /**
  * DefaultOAuth2User를 상속하고, email과 role 필드를 추가로 가진다.
  */
+
 @Getter
 public class CustomOAuth2User extends DefaultOAuth2User {
 
 	private String email;
 	private Role role;
 
-	/**
-	 * Constructs a {@code DefaultOAuth2User} using the provided parameters.
-	 *
-	 * @param authorities      the authorities granted to the user
-	 * @param attributes       the attributes about the user
-	 * @param nameAttributeKey the key used to access the user's "name" from
-	 *                         {@link #getAttributes()}
-	 */
 	public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
 		Map<String, Object> attributes, String nameAttributeKey,
 		String email, Role role) {
