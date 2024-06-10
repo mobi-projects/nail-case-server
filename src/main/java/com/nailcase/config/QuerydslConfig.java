c87606475827f28e4883e9a647bf23b8b7cf1622
qushe8r <qushe8r@gmail.com>
2024-06-10 15:40:29 +0900

    :sparkles: feat: QuerydslConfig (NAILCASE-80)

---
+++ src/main/java/com/nailcase/config/QuerydslConfig.java
@@ -0,0 +1,20 @@
+package com.nailcase.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class QuerydslConfig {
+	private final EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
