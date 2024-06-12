75a5d96357c407bb36c85a26be3d4395f025c788
YoonSung Kim <opqudk@gmail.com>
2024-06-12 17:51:14 +0900

    :card_file_box: chore: Add shop entity (NAILCASE-96)
    - shop entity 추가
    - member에 shop join 추가
    
    resoling: [NAILCASE-96](https://nailcase.atlassian.net/browse/NAILCASE-96)
    ref: [NAILCASE-94](https://nailcase.atlassian.net/browse/NAILCASE-94)

--- src/main/java/com/nailcase/model/entity/Member.java
+++ src/main/java/com/nailcase/model/entity/Member.java
@@ -1,16 +1,21 @@
 package com.nailcase.model.entity;
 
+import java.util.List;
+
 import com.nailcase.common.BaseEntity;
 import com.nailcase.model.enums.Role;
 import com.nailcase.model.enums.SocialType;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -56,6 +61,9 @@ public class Member extends BaseEntity {
 
 	@Column(name = "profileImgUrl", length = 128)
 	private String profileImgUrl;
+
+	@OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Shop> shopList;
 
 	// TODO
 	public Member update(String name) {
---
+++ src/main/java/com/nailcase/model/entity/Shop.java
@@ -0,0 +1,42 @@
+package com.nailcase.model.entity;
+
+import com.nailcase.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shops")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Shop extends BaseEntity {
+
+	@Id
+	@Column(name = "shop_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long shopId;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "owner_id", referencedColumnName = "member_id")
+	private Member member;
+
+	@Column(name = "shop_name", nullable = false, length = 128)
+	private String shopName;
+
+	@Column(name = "phone", nullable = false, length = 16)
+	private String phone;
+
+	@Column(name = "overview", length = 2048)
+	private String overview;
+
+	@Column(name = "address", length = 128)
+	private String address;
+}
