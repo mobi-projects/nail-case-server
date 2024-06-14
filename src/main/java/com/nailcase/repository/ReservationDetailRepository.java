83ef8f7f9ccfc48a0fd012444f7f0578834207d4
qushe8r <qushe8r@gmail.com>
2024-06-14 13:13:26 +0900

    :sparkles: feat: add ReservationRepository (NAILCASE-93)
    - ReservationRepository
    - ReservationDetailRepository

---
+++ src/main/java/com/nailcase/repository/ReservationDetailRepository.java
@@ -0,0 +1,8 @@
+package com.nailcase.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nailcase.model.entity.ReservationDetail;
+
+public interface ReservationDetailRepository extends JpaRepository<ReservationDetail, Long> {
+}
