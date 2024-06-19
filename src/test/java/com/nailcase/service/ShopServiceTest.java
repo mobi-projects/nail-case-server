ed65f04793bea0b6b99f33a99997a3b5caced8f8
YoonSung Kim <opqudk@gmail.com>
2024-06-19 16:34:10 +0900

    :white_check_mark: test(unit): Add getShopById (NAILCASE-96)
    - 성공, 실패 테스트 추가
    - 비교 공통 함수 추가
    
    resoling: [NAILCASE-96](https://nailcase.atlassian.net/browse/NAILCASE-96), [NAILCASE-144](https://nailcase.atlassian.net/browse/NAILCASE-144)
    refs: [NAILCASE-94](https://nailcase.atlassian.net/browse/NAILCASE-94),
    [NAILCASE-128](https://nailcase.atlassian.net/browse/NAILCASE-128)

--- src/test/java/com/nailcase/service/ShopServiceTest.java
+++ src/test/java/com/nailcase/service/ShopServiceTest.java
@@ -12,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.nailcase.exception.BusinessException;
+import com.nailcase.exception.codes.ShopErrorCode;
 import com.nailcase.mapper.ShopMapper;
 import com.nailcase.model.dto.ShopDto;
 import com.nailcase.model.entity.Member;
@@ -44,7 +46,7 @@ public class ShopServiceTest {
 	private final ShopMapper shopMapper = ShopMapper.INSTANCE;
 
 	@Test
-	@DisplayName("샵 동록시 ROLE MANAGER | shop 저장, shopInfo, shopHour 초기화 된다.")
+	@DisplayName("샵 동록시 ROLE MANAGER | shop 저장, shopInfo, shopHour 초기화 성공 테스트")
 	void registerShopSuccess() throws Exception {
 		// Given
 		Shop shop = shopFixture.getShop();
@@ -60,15 +62,7 @@ public class ShopServiceTest {
 		ShopDto.Response result = shopService.registerShop(request, memberId);
 
 		// Then
-		assertEquals(response.getShopId(), result.getShopId());
-		assertEquals(response.getOwnerId(), result.getOwnerId());
-		assertEquals(response.getShopName(), result.getShopName());
-		assertEquals(response.getPhone(), result.getPhone());
-		assertEquals(response.getOverview(), result.getOverview());
-		assertEquals(response.getAddress(), result.getAddress());
-		assertEquals(response.getAvailableSeats(), result.getAvailableSeats());
-		assertEquals(response.getCreatedAt(), result.getCreatedAt());
-		assertEquals(response.getModifiedAt(), result.getModifiedAt());
+		assertTrue(equals(response, result));
 
 		verify(memberRepository).findById(memberId);
 		assertEquals(Role.MANAGER, member.getRole());
@@ -76,5 +70,56 @@ public class ShopServiceTest {
 		verify(shopRepository, times(1)).save(any(Shop.class));
 		verify(shopInfoService, times(1)).initShopInfo(any(Shop.class));
 		verify(shopHourService, times(1)).initShopHour(any(Shop.class));
+	}
+
+	@Test
+	@DisplayName("getShopById 성공 테스트")
+	void getShopByIdSuccess() throws BusinessException {
+		// Given
+		Shop shop = shopFixture.getShop();
+		Long shopId = shop.getShopId();
+		ShopDto.Response response = shopMapper.toResponse(shop);
+
+		when(shopRepository.findById(shopId)).thenReturn(Optional.of(shop));
+
+		// When
+		ShopDto.Response result = shopService.getShopById(shopId);
+
+		// Then
+		assertTrue(equals(response, result));
+
+		// Verify interactions with mocks
+		verify(shopRepository, times(1)).findById(shopId);
+	}
+
+	@Test
+	@DisplayName("getShopById 실패 테스트 - ShopNotFound")
+	void getShopByIdFailShopNotFound() {
+		// Given
+		Long shopId = 1L;
+		when(shopRepository.findById(shopId)).thenReturn(Optional.empty());
+
+		// When
+		BusinessException exception = assertThrows(BusinessException.class, () -> {
+			shopService.getShopById(shopId);
+		});
+
+		// Then
+		assertEquals(ShopErrorCode.SHOP_NOT_FOUND, exception.getErrorCode());
+
+		// Verify interactions with mocks
+		verify(shopRepository, times(1)).findById(shopId);
+	}
+
+	private boolean equals(ShopDto.Response expected, ShopDto.Response actual) {
+		return expected.getShopId().equals(actual.getShopId()) &&
+			expected.getOwnerId().equals(actual.getOwnerId()) &&
+			expected.getShopName().equals(actual.getShopName()) &&
+			expected.getPhone().equals(actual.getPhone()) &&
+			expected.getOverview().equals(actual.getOverview()) &&
+			expected.getAddress().equals(actual.getAddress()) &&
+			expected.getAvailableSeats().equals(actual.getAvailableSeats()) &&
+			expected.getCreatedAt().equals(actual.getCreatedAt()) &&
+			expected.getModifiedAt().equals(actual.getModifiedAt());
 	}
 }
