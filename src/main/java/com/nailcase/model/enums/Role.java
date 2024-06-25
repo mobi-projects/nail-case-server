f790f1fd4d305fbf995f38d046bd93cddf40253a
jangdm37 <jangdm37@gmail.com>
2024-06-25 13:34:02 +0900

    ️✨ feat(fake-login-for-demo)(NAILCASE-194)
    - 데모 로그인
    - 서버 시작시 계정 정보 삽입
    
    resolves: [NAILCASE-194](https://nailcase.atlassian.net/browse/NAILCASE-194))

--- src/main/java/com/nailcase/model/enums/Role.java
+++ src/main/java/com/nailcase/model/enums/Role.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 public enum Role {
 	OWNER("ROLE_SHOP_OWNER"),
 	MANAGER("ROLE_SHOP_MANAGER"),
-	USER("ROLE_SHOP_USER"),
-	GUEST("ROLE_GUEST");
-	
+	USER("ROLE_SHOP_USER");
 	private final String key;
 }
