25cc2e1d0d352a40243a1d6161c209a4ef676dc0
YoonSung Kim <opqudk@gmail.com>
2024-06-28 09:59:58 +0900

    :bug: fix: Mapper (NAILCASE-146)
    resoling: [NAILCASE-146](https://nailcase.atlassian.net/browse/NAILCASE-146)
    refs: [NAILCASE-128](https://nailcase.atlassian.net/browse/NAILCASE-128)'

--- src/main/java/com/nailcase/mapper/WorkHourMapper.java
+++ src/main/java/com/nailcase/mapper/WorkHourMapper.java
@@ -1,15 +1,30 @@
 package com.nailcase.mapper;
 
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.nailcase.model.dto.WorkHourDto;
 import com.nailcase.model.entity.WorkHour;
+import com.nailcase.util.DateUtils;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+	componentModel = "spring",
+	imports = {DateUtils.class},
+	injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
 public interface WorkHourMapper {
 
 	WorkHourMapper INSTANCE = Mappers.getMapper(WorkHourMapper.class);
 
+	@Mapping(
+		target = "openTime",
+		expression = "java(DateUtils.localDateTimeToUnixTimeStamp(workHour.getOpenTime()))"
+	)
+	@Mapping(
+		target = "closeTime",
+		expression = "java(DateUtils.localDateTimeToUnixTimeStamp(workHour.getCloseTime()))"
+	)
 	WorkHourDto.Put toPutResponse(WorkHour workHour);
 }
