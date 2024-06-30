b5de8718d7654668135fde4d39b02e7e2ad83078
YoonSung Kim <opqudk@gmail.com>
2024-06-30 22:00:51 +0900

    :card_file_box: chore: shop info entity (NAILCASE-145)
    resolving: [NAILCASE-145](https://nailcase.atlassian.net/browse/NAILCASE-145)
    ref: [NAILCASE-128](https://nailcase.atlassian.net/browse/NAILCASE-128)

---
+++ src/main/java/com/nailcase/model/dto/ShopInfoDto.java
@@ -0,0 +1,22 @@
+﻿package com.nailcase.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class ShopInfoDto {
+
+	@Data
+	@NoArgsConstructor
+	public static class Address {
+	}
+
+	@Data
+	@NoArgsConstructor
+	public static class Info {
+	}
+
+	@Data
+	@NoArgsConstructor
+	public static class Price {
+	}
+}
