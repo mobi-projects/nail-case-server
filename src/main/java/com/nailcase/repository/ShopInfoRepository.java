d83de57564215388d2623e9e870a47827c67c8f8
YoonSung Kim <opqudk@gmail.com>
2024-07-04 17:54:21 +0900

    :sparkles: feat: Add updateAddress (NAILCASE-145)
    resoling: [NAILCASE-145](https://nailcase.atlassian.net/browse/NAILCASE-145)
    refs: [NAILCASE-128](https://nailcase.atlassian.net/browse/NAILCASE-128)'

--- src/main/java/com/nailcase/repository/ShopInfoRepository.java
+++ src/main/java/com/nailcase/repository/ShopInfoRepository.java
@@ -1,5 +1,7 @@
 package com.nailcase.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.nailcase.model.entity.ShopInfo;
 
 @Repository
 public interface ShopInfoRepository extends JpaRepository<ShopInfo, Long> {
+	Optional<ShopInfo> findByShopId(Long shopId);
 }
