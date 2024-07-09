906735b5a1696e7e897ec5e8d84cc336fbffea39
jangdm37 <jangdm37@gmail.com>
2024-07-09 19:02:29 +0900

    ️✨feat: kakao-login(NAILCASE-280) manager-login-api 추가 및 카카오 로그인 api 리팩토링 (NAILCASE-280)
    - manager-login-api 추가
    - 카카오 로그인 api 리팩토링
    
    resolve: [NAILCASE-280](https://nailcase.atlassian.net/browse/NAILCASE-280)

--- src/main/java/com/nailcase/controller/DemoLoginController.java
+++ src/main/java/com/nailcase/controller/DemoLoginController.java
@@ -10,9 +10,12 @@ import com.nailcase.exception.BusinessException;
 import com.nailcase.exception.codes.UserErrorCode;
 import com.nailcase.jwt.JwtService;
 import com.nailcase.model.dto.MemberDto;
+import com.nailcase.model.dto.NailArtistDto;
 import com.nailcase.model.entity.Member;
+import com.nailcase.model.entity.NailArtist;
 import com.nailcase.model.enums.UserType;
 import com.nailcase.repository.MemberRepository;
+import com.nailcase.repository.NailArtistRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class DemoLoginController {
 	private final JwtService jwtService;
 	private final MemberRepository memberRepository;
+	private final NailArtistRepository nailArtistRepository;
 
 	@GetMapping(value = "/member", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<MemberDto> loginUser() {
@@ -40,12 +44,12 @@ public class DemoLoginController {
 	}
 
 	@GetMapping(value = "/admin", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<MemberDto> loginAdmin() {
-		Member admin = memberRepository.findByEmail("manager@example.com")
+	public ResponseEntity<NailArtistDto> loginAdmin() {
+		NailArtist admin = nailArtistRepository.findByEmail("manager@example.com")
 			.orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
 
-		MemberDto adminDto = MemberDto.fromEntity(admin);
-		String accessToken = jwtService.createAccessToken(admin.getEmail(), admin.getMemberId(),
+		NailArtistDto adminDto = NailArtistDto.fromEntity(admin);
+		String accessToken = jwtService.createAccessToken(admin.getEmail(), admin.getNailArtistId(),
 			UserType.MANAGER.getValue());
 		String refreshToken = jwtService.createRefreshToken(admin.getEmail(), UserType.MANAGER.getValue());
 
