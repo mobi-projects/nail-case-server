803a47ebd2572a282ad81b575902e41a2c06ab7a
jangdm37 <jangdm37@gmail.com>
2024-07-19 20:24:16 +0900

    ♻️ refactor(refactor-image-api)(NAILCASE-294)
    - post api에 비동기 처리 추가
    
    resolves: [NAILCASE-294](https://nailcase.atlassian.net/browse/NAILCASE-294))

--- src/main/java/com/nailcase/controller/MonthlyArtController.java
+++ src/main/java/com/nailcase/controller/MonthlyArtController.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class MonthlyArtController {
 	private final MonthlyArtService monthlyArtService;
-
+	
 	// @PostMapping("/images")
 	// @ResponseStatus(HttpStatus.CREATED)
 	// public List<MonthlyArtImageDto> uploadImages(
