ecae1fc52046c138de69d2bc25fbf8e741908225
jangdm37 <jangdm37@gmail.com>
2024-07-27 13:04:08 +0900

    ✨ feat(shop-register-api)(NAILCASE-364)
    - shop 등록에서 string 값과 동시에 받아오기
    - shop 등록시 jsonMapper 사용후 리팩토링
    
    resolves: [NAILCASE-364](https://nailcase.atlassian.net/browse/NAILCASE-364))

--- src/main/java/com/nailcase/exception/codes/ShopErrorCode.java
+++ src/main/java/com/nailcase/exception/codes/ShopErrorCode.java
@@ -7,7 +7,8 @@ public enum ShopErrorCode implements ErrorCodeInterface {
 	SHOP_NOT_FOUND(1501, "샾을 찾을 수 없음"),
 	SHOP_DELETION_FORBIDDEN(1502, "샵 삭제 권한이 없음"),
 	LIKE_NOT_FOUND(1503, "좋아요한 샵을 찾을 수 없음"),
-	REGISTRATION_FAILED(1504, "샵 등록 실패");
+	REGISTRATION_FAILED(1504, "샵 등록 실패"),
+	INVALID_SHOP_DATA(1505, "샵 등록 시 부적절한 데이터로 인한 실패");
 
 	private final int code;
 	private final String message;
