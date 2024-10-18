b1d79f4cdbf40664db8713c15790a3219b4b422c
jangdm37 <jangdm37@gmail.com>
2024-10-18 20:19:37 +0900

    chatting finish2

--- src/main/java/com/nailcase/config/rabbitMq/RabbitMQConfig.java
+++ src/main/java/com/nailcase/config/rabbitMq/RabbitMQConfig.java
@@ -9,6 +9,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +22,18 @@ public class RabbitMQConfig {
 	public static final String CHAT_EXCHANGE_NAME = "chat.exchange";
 	public static final String CHAT_QUEUE_NAME = "chat.queue";
 	public static final String CHAT_ROUTING_KEY = "shop.*.room.#";
+
+	@Value("${spring.rabbitmq.host}")
+	private String rabbitHost;
+
+	@Value("${spring.rabbitmq.port}")
+	private int rabbitPort;
+
+	@Value("${spring.rabbitmq.username}")
+	private String rabbitUsername;
+
+	@Value("${spring.rabbitmq.password}")
+	private String rabbitPassword;
 
 	@Bean
 	public Queue queue() {
@@ -55,10 +68,10 @@ public class RabbitMQConfig {
 	@Bean
 	public ConnectionFactory connectionFactory() {
 		CachingConnectionFactory factory = new CachingConnectionFactory();
-		factory.setHost("localhost");
-		factory.setPort(5672);  // AMQP 기본 포트
-		factory.setUsername("guest");
-		factory.setPassword("guest");
+		factory.setHost(rabbitHost);
+		factory.setPort(rabbitPort);
+		factory.setUsername(rabbitUsername);
+		factory.setPassword(rabbitPassword);
 		return factory;
 	}
 
