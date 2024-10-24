96806256b203ec38b3f94cef1340e5391e7dc191
jangdm37 <jangdm37@gmail.com>
2024-10-24 10:54:45 +0900

    chatting port fix2

--- src/main/java/com/nailcase/config/chatting/StompWebSocketConfig.java
+++ src/main/java/com/nailcase/config/chatting/StompWebSocketConfig.java
@@ -25,7 +25,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/stomp/chat")
+		registry.addEndpoint("/api/v1/stomp/chat")  // context-path를 포함한 전체 경로
 			.setAllowedOrigins(
 				"http://localhost:3000",
 				"https://nail-case-client.vercel.app",
