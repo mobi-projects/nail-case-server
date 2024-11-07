8af6e0296896a8a93fadbb3a02419f11c1d8319c
jangdm37 <jangdm37@gmail.com>
2024-11-07 17:20:10 +0900

    chatting 에러 수정

--- src/main/java/com/nailcase/repository/NotificationQuerydslRepositoryImpl.java
+++ src/main/java/com/nailcase/repository/NotificationQuerydslRepositoryImpl.java
@@ -1,10 +1,5 @@
 package com.nailcase.repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.stereotype.Repository;
-
 import com.nailcase.model.entity.Notification;
 import com.nailcase.model.entity.QNotification;
 import com.nailcase.model.enums.NotificationType;
@@ -12,96 +7,99 @@ import com.nailcase.model.enums.Role;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class NotificationQuerydslRepositoryImpl implements NotificationQuerydslRepository {
 
-	private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
-	@Override
-	public List<Notification> findByMemberReceiverId(Long memberId) {
-		QNotification notification = QNotification.notification;
+    @Override
+    public List<Notification> findByMemberReceiverId(Long memberId) {
+        QNotification notification = QNotification.notification;
 
-		// 멤버가 받는 알림 조건 설정
-		Predicate condition = notification.receiverId.eq(memberId)
-			.and(notification.notificationType.in(NotificationType.RESERVATION_APPROVE,
-				NotificationType.RESERVATION_REJECT));
+        // 멤버가 받는 알림 조건 설정
+        Predicate condition = notification.receiverId.eq(memberId)
+                .and(notification.notificationType.in(NotificationType.RESERVATION_APPROVE,
+                        NotificationType.RESERVATION_REJECT, NotificationType.NEW_CHAT_MESSAGE_TO_MEMBER));
 
-		// 쿼리 실행
+        // 쿼리 실행
 
-		// 페이징 처리
-		return queryFactory.selectFrom(notification)
-			.where(condition)
-			.leftJoin(notification.reservationDetail).fetchJoin()
-			.limit(15)
-			.orderBy(notification.createdAt.desc())
-			.fetch();
-	}
+        // 페이징 처리
+        return queryFactory.selectFrom(notification)
+                .where(condition)
+                .leftJoin(notification.reservationDetail).fetchJoin()
+                .limit(15)
+                .orderBy(notification.createdAt.desc())
+                .fetch();
+    }
 
-	@Override
-	public List<Notification> findByNailArtistReceiverId(Long nailArtistId) {
-		QNotification notification = QNotification.notification;
+    @Override
+    public List<Notification> findByNailArtistReceiverId(Long nailArtistId) {
+        QNotification notification = QNotification.notification;
 
-		// 네일 아티스트가 받는 알림 조건 설정
-		Predicate condition = notification.receiverId.eq(nailArtistId)
-			.and(notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
-				NotificationType.RESERVATION_CANCEL));
+        // 네일 아티스트가 받는 알림 조건 설정
+        Predicate condition = notification.receiverId.eq(nailArtistId)
+                .and(notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
+                        NotificationType.RESERVATION_CANCEL, NotificationType.NEW_CHAT_MESSAGE_TO_SHOP));
 
-		// 페이징 처리
-		return queryFactory.selectFrom(notification)
-			.where(condition)
-			.leftJoin(notification.reservationDetail).fetchJoin()
-			.limit(15)
-			.orderBy(notification.createdAt.desc())
-			.fetch();
-	}
+        // 페이징 처리
+        return queryFactory.selectFrom(notification)
+                .where(condition)
+                .leftJoin(notification.reservationDetail).fetchJoin()
+                .limit(15)
+                .orderBy(notification.createdAt.desc())
+                .fetch();
+    }
 
-	@Override
-	public List<Notification> findByTypeAndReceiverIdWithNotRead(Long receiverId, Role role) {
-		QNotification notification = QNotification.notification;
-		BooleanExpression condition = role == Role.MANAGER ?
-			notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
-				NotificationType.RESERVATION_CANCEL) :
-			notification.notificationType.in(NotificationType.RESERVATION_APPROVE, NotificationType.RESERVATION_REJECT
-				, NotificationType.RESERVATION_COMPLETE);
+    @Override
+    public List<Notification> findByTypeAndReceiverIdWithNotRead(Long receiverId, Role role) {
+        QNotification notification = QNotification.notification;
+        BooleanExpression condition = role == Role.MANAGER ?
+                notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
+                        NotificationType.RESERVATION_CANCEL) :
+                notification.notificationType.in(NotificationType.RESERVATION_APPROVE, NotificationType.RESERVATION_REJECT
+                        , NotificationType.RESERVATION_COMPLETE);
 
-		return queryFactory.selectFrom(notification)
-			.where(notification.receiverId.eq(receiverId)
-					.and(condition),
-				notification.isRead.eq(false)
-			)
-			.join(notification.reservationDetail).fetchJoin()
-			.orderBy(notification.createdAt.desc())
-			.fetch();
-	}
+        return queryFactory.selectFrom(notification)
+                .where(notification.receiverId.eq(receiverId)
+                                .and(condition),
+                        notification.isRead.eq(false)
+                )
+                .join(notification.reservationDetail).fetchJoin()
+                .orderBy(notification.createdAt.desc())
+                .fetch();
+    }
 
-	@Override
-	public void updateReadStatusInNotReadNotification(Long receiverId, Role role) {
-		QNotification notification = QNotification.notification;
-		queryFactory.update(notification)
-			.set(notification.isRead, true)
-			.where(notification.isRead.eq(false)
-				, notification.receiverId.eq(receiverId)
-				, role == Role.MANAGER ?
-					notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
-						NotificationType.RESERVATION_CANCEL) :
-					notification.notificationType.in(NotificationType.RESERVATION_APPROVE,
-						NotificationType.RESERVATION_REJECT,
-						NotificationType.RESERVATION_COMPLETE)
-			)
-			.execute();
-	}
+    @Override
+    public void updateReadStatusInNotReadNotification(Long receiverId, Role role) {
+        QNotification notification = QNotification.notification;
+        queryFactory.update(notification)
+                .set(notification.isRead, true)
+                .where(notification.isRead.eq(false)
+                        , notification.receiverId.eq(receiverId)
+                        , role == Role.MANAGER ?
+                                notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
+                                        NotificationType.RESERVATION_CANCEL) :
+                                notification.notificationType.in(NotificationType.RESERVATION_APPROVE,
+                                        NotificationType.RESERVATION_REJECT,
+                                        NotificationType.RESERVATION_COMPLETE)
+                )
+                .execute();
+    }
 
-	@Override
-	public Optional<Notification> findByReservationDetailId(Long reservationDetailId) {
-		QNotification notification = QNotification.notification;
-		return Optional.ofNullable(queryFactory.selectFrom(notification)
-			.where(notification.reservationDetail.reservationDetailId.eq(reservationDetailId))
-			.fetchOne());
-	}
+    @Override
+    public Optional<Notification> findByReservationDetailId(Long reservationDetailId) {
+        QNotification notification = QNotification.notification;
+        return Optional.ofNullable(queryFactory.selectFrom(notification)
+                .where(notification.reservationDetail.reservationDetailId.eq(reservationDetailId))
+                .fetchOne());
+    }
 
 }
 
