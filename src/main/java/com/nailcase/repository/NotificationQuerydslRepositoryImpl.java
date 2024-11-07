b9ac03e6287f05dc3a5c0bfb0355a264dd973759
jangdm37 <jangdm37@gmail.com>
2024-11-07 17:23:30 +0900

    chatting 에러 수정3

--- src/main/java/com/nailcase/repository/NotificationQuerydslRepositoryImpl.java
+++ src/main/java/com/nailcase/repository/NotificationQuerydslRepositoryImpl.java
@@ -85,10 +85,10 @@ public class NotificationQuerydslRepositoryImpl implements NotificationQuerydslR
                         , notification.receiverId.eq(receiverId)
                         , role == Role.MANAGER ?
                                 notification.notificationType.in(NotificationType.RESERVATION_REQUEST,
-                                        NotificationType.RESERVATION_CANCEL) :
+                                        NotificationType.RESERVATION_CANCEL, NotificationType.NEW_CHAT_MESSAGE_TO_MEMBER) :
                                 notification.notificationType.in(NotificationType.RESERVATION_APPROVE,
                                         NotificationType.RESERVATION_REJECT,
-                                        NotificationType.RESERVATION_COMPLETE)
+                                        NotificationType.RESERVATION_COMPLETE, NotificationType.NEW_CHAT_MESSAGE_TO_SHOP)
                 )
                 .execute();
     }
